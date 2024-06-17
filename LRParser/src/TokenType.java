d9b5d9f48b9579cde5cbcfd59a3f828b703f11cd
dana <22100409@handong.ac.kr>
2024-06-17 20:24:41 +0900

    added lr1

--- LRParser/src/TokenType.java
+++ LRParser/src/TokenType.java
@@ -1,10 +1,9 @@
-package smalllexer;
 
 public class TokenType {
     public static final String IDENTIFIER = "identifier";
-    public static final String NUM_LITERAL = "number literal";
-    public static final String STRING_LITERAL = "string literal";
-    public static final String COMMA = "punctuation - comma";
+    public static final String NUM_LITERAL = "number_literal";
+    public static final String STRING_LITERAL = "string_literal";
+    public static final String COMMA = "punctuation comma";
     public static final String PLUS_OP = "plus operator";
     public static final String MUL_OP = "multiplication operator";
     public static final String ASSIGNMENT_OP = "assignment operator";
@@ -14,5 +13,6 @@ public class TokenType {
     public static final String STATEMENT_TERM = "statement terminator";
     public static final String KEYWORDS = "keyword";
     public static final String COMMENTS = "comment";
+    public static final String EQ_OP = "equals operator";
+    public static final String INCREMENT = "increment operator";
 }
-
